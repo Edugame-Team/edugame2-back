456da1bf8457ed6990efdf8310774db4d494c473
fahim BENCHAABANE <fahim@jump-biz.com>
2020-10-16 16:39:28 +0200

    update variable for deploy and db creation/update

--- server/utils/dbConnection.js
+++ server/utils/dbConnection.js
@@ -1,16 +1,16 @@
 const sequelize = require('sequelize');
 require('dotenv').config();
 
-console.log(process.env.DEV_DB_DATABASE);
+console.log(process.env.DB_DATABASE);
 
 const dbConnection = new sequelize.Sequelize(
-  process.env.DEV_DB_DATABASE,
-  process.env.DEV_DB_USERNAME,
-  process.env.DEV_DB_PASSWORD,
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DEV_DB_HOST,
-    dialect: process.env.DEV_DB_DIALECT,
-    port: process.env.DEV_DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
   },
 );
 
